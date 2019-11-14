97ce148b6e73cf77b39a1e51781a61754ae9855c
naofumi-fujii <naofumi.fujii@gmail.com>
2019-11-14 17:46:25 +0900

    sftp upload

--- lib/b_dash.rb
+++ lib/b_dash.rb
@@ -2,5 +2,22 @@ require "b_dash/version"
 
 module BDash
   class Error < StandardError; end
+
   # Your code goes here...
+
+  class SFTP
+    def self.upload!(username:, hostname:, port:, local_filepath:)
+      option = {
+        keys: "~/.ssh/id_rsa",
+        passphrase: "",
+        port: port,
+      }
+
+      Net::SFTP.start(hostname, username, option) do |sftp|
+        dest = "input/#{File.basename(local_filepath)}"
+        puts "#{local_filepath} -> #{dest}"
+        sftp.upload!(local_filepath, dest)
+      end
+    end
+  end
 end
